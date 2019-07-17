8ef49ea71378e633b4e800a0ae0289138339134c
Elliot Bobillet <elliot.bobillet@hotmail.fr>
2019-07-17 16:28:16 +0200

    ajout de ngFor dans la page animau-artistes, réception de la data fonctionelle

--- wildCircus/src/app/animaux/animaux.component.ts
+++ wildCircus/src/app/animaux/animaux.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../service/admin.service';
 
 @Component({
   selector: 'app-animaux',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animaux.component.scss']
 })
 export class AnimauxComponent implements OnInit {
+  artists = []
+  newArtist = {
+    id : '',
+    name : '',
+    role: ''
+  }
 
-  constructor() { }
+  constructor(private connection: AdminService) { }
 
   ngOnInit() {
+    this.connection.getArtists().subscribe((data: any) => {
+      console.log(data)
+      this.artists = data
+    })
   }
 
 }
